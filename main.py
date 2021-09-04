115529ff1d01a2fd0409aa6656a620a5f06bda3a
Weiliang Hu <wh153@duke.edu>
2021-09-04 01:51:17 +0000

    add_exp

--- main.py
+++ main.py
@@ -14,5 +14,13 @@ async def add(num1: int, num2: int):
     total = num1 + num2
     return {"total": total}
 
+@app.get("/exponent/{num1}/{num2}")
+async def exponent (num1: int, num2: int):
+    """Get the power num2 of num1"""
+    
+        
+    result = num1 ** num2
+    return {"result": result}
+
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
